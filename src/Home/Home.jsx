6178510c2495cc305f02cfbd7e5ea1f2a1865621
Samir <samir81@live.com>
2026-04-10 01:56:25 +0300

    finished from Home page

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -3,6 +3,7 @@ import Shop from "../Shop/Shop.jsx";
 import { products } from "../Shop/Shop.jsx";
 import "./Homestyle.css";
 import { useState, useEffect } from "react";
+import Logo from "../assets/images/Logo.png";
 import Hero from "../Home/images/category1.jpg";
 import Hero2 from "../Home/images/category2.jpg";
 import Hero3 from "../Home/images/category3.jpg";
@@ -20,6 +21,30 @@ import banner from "../Home/images/banner1.jpg";
 import discount from "../Home/images/discount.jpg";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import {
+  FaCcAmazonPay,
+  FaCcMastercard,
+  FaGooglePay,
+  FaCcPaypal,
+  FaCcApplePay,
+  FaCcStripe,
+  FaInstagram,
+  FaFacebook,
+  FaTwitter,
+  FaYoutube,
+  FaPinterest,
+} from "react-icons/fa";
+import {
+  FireTruckSharp,
+  Headphones,
+  HeadsetMic,
+  Money,
+} from "@mui/icons-material";
+import insta1 from "../Home/images/insta1.jpg";
+import insta2 from "../Home/images/insta2.jpg";
+import insta3 from "../Home/images/insta3.jpg";
+import insta4 from "../Home/images/insta4.jpg";
+import insta5 from "../Home/images/insta5.jpg";
 
 function Home() {
   const [hotTrends, setHotTrends] = useState([]);
@@ -499,20 +524,226 @@ function Home() {
           <div className="col-md-6">
             <div className="d-flex">
               <div>
-              <img src={discount} alt="" />
-            </div>
-            <div className="col-md-6 bg-light w-100">
-              <div className="text-center discountContent">
-                <p>DISCOUNT</p>
-                <h2>Summer 2030</h2><span className="circle"></span>
-                <span>SALE</span>
-                <span className="text-danger">  50%</span>
+                <img src={discount} alt="" />
               </div>
-            </div>
+              <div className="col-md-6 w-100 colBG">
+                <div className="text-center discountContent ">
+                  <p className="fs-3">DISCOUNT</p>
+                  <span className="circle"></span>
+                  <h2 className="fw-bold text-danger fs-1 summer">
+                    Summer 2030
+                  </h2>
+                  <span className="fs-3">SALE</span>
+                  <span className="fs-2 text-danger"> 50%</span>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
+
+      {/* benefits WHY us section */}
+      <div className="container mt-5">
+        <div className="row justify-content-center">
+          <div className="col-md-3">
+            <div className="d-flex align-items-center justify-content-center">
+              <FireTruckSharp sx={{ fontSize: 60, color: "red" }} />
+              <div>
+                <p className="fw-bold">Free Shipping</p>
+                <p className="text-muted">for all order over $99</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="d-flex align-items-center justify-content-center">
+              <Money sx={{ fontSize: 60, color: "red" }} />
+              <div>
+                <p className="fw-bold">Money Back Guarantee</p>
+                <p className="text-muted">If good have Problems</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="d-flex align-items-center justify-content-center">
+              <HeadsetMic sx={{ color: "red", fontSize: 60 }} />
+              <div>
+                <p className="fw-bold">Online Support 24/7</p>
+                <p className="text-muted">Dedicated support</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="d-flex align-items-center justify-content-center">
+              <Headphones sx={{ fontSize: 60, color: "red" }} />
+              <div>
+                <p className="fw-bold">Payment Secure</p>
+                <p className="text-muted">100% secure payment</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Gallery Instagram section */}
+      <div className="containerGallery mt-5">
+        <div className="gallery-item">
+          <img src={insta1} alt="Instagram 1" className="gallery-img" />
+          <div className="gallery-overlay">
+            <FaInstagram className="insta-icon"  style={{ color: '#E1306C' }} />
+          </div>
+        </div>
+
+        <div className="gallery-item">
+          <img src={insta2} alt="Instagram 2" className="gallery-img"  style={{ color: '#E1306C' }} />
+          <div className="gallery-overlay">
+            <FaInstagram className="insta-icon"  style={{ color: '#E1306C' }} />
+          </div>
+        </div>
+        <div className="gallery-item">
+          <img src={insta3} alt="Instagram 3" className="gallery-img" style={{ color: '#E1306C' }}  />
+          <div className="gallery-overlay">
+            <FaInstagram className="insta-icon"  style={{ color: '#E1306C' }} />
+          </div>
+        </div>
+
+        <div className="gallery-item">
+          <img src={insta4} alt="Instagram 4" className="gallery-img" />
+          <div className="gallery-overlay">
+            <FaInstagram className="insta-icon"  style={{ color: '#E1306C' }} />
+          </div>
+        </div>
+        <div className="gallery-item">
+          <img src={insta5} alt="Instagram 5" className="gallery-img" />
+          <div className="gallery-overlay">
+            <FaInstagram className="insta-icon" style={{ color: '#E1306C' }}  />
+          </div>
+        </div>
+      </div>
+      <footer className="mt-5">
+        <div className="container mt-5">
+          <div className="row">
+            <div className="col-lg-4 col-md-6 col-sm 7">
+              <div className="footerAbout">
+                <div className="footerLogo">
+                  <img src={Logo} alt="" />
+                </div>
+                <p className="FooterAboutText">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt
+                </p>
+                <div className="footerPayment">
+                  <FaCcAmazonPay />
+                  <FaCcMastercard />
+                  <FaGooglePay />
+                  <FaCcPaypal />
+                  <FaCcApplePay />
+                  <FaCcStripe />
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-2 col-md-3 col-sm-5">
+              <div className="footerWidget">
+                <h6>Quick Links</h6>
+                <ul>
+                  <li>
+                    About
+                    <a href="#"></a>
+                  </li>
+                  <li>
+                    Blogs
+                    <a href="#"></a>
+                  </li>
+                  <li>
+                    Contact
+                    <a href="#"></a>
+                  </li>
+                  <li>
+                    FAQ
+                    <a href="#"></a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="col-lg-2 col-md-3 col-sm-4">
+              <div className="footerWidget">
+                <h6>Account</h6>
+                <ul>
+                  <li>
+                    my Account <a href="#"></a>
+                  </li>
+                  <li>
+                    Orders Trucking
+                    <a href="#"></a>
+                  </li>
+                  <li>
+                    Checkout
+                    <a href="#"></a>
+                  </li>
+                  <li>
+                    WishList
+                    <a href="#"></a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="col-lg-4 col-md-8 col-sm-8">
+              <div className="footerNewsLetter">
+                <h6>NewsLetter</h6>
+                <form action="">
+                  <label
+                    htmlFor="newsletter-email"
+                    className="visually-hidden"
+                    novalidate
+                  >
+                    email address
+                  </label>
+                  <div className="input-group">
+                    <input
+                      className="form-control"
+                      type="email"
+                      name="email"
+                      placeholder="Enter your Email"
+                      required
+                      aria-required="true"
+                      autoComplete="email"
+                    />
+                    <button className="btn btn-primary form-control">
+                      Subscribe
+                    </button>
+                  </div>
+                </form>
+              </div>
+
+              <div className="footerSocial">
+                <a href="#" role="listitem" aria-label="Facebook">
+                  <FaFacebook />
+                </a>
+                <a href="#" role="listitem" aria-label="X (Twitter)">
+                  <FaTwitter />
+                </a>
+                <a href="#" role="listitem" aria-label="YouTube">
+                  <FaInstagram />
+                </a>
+                <a href="#" role="listitem" aria-label="Instagram">
+                  <FaYoutube />
+                </a>
+                <a href="#" role="listitem" aria-label="Pintrest">
+                  <FaPinterest />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <br />
+        <hr />
+        <br />
+        <p className="text-center lead">
+          Copyright &copy; 2026 All rights reserved | This template is made with
+          by Eng. Samir
+        </p>
+      </footer>
     </div>
   );
 }
